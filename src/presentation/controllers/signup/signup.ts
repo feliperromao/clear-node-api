681e3c3cd05b8130dd58c0e423a638585b90af54
Felipe <felipe.martins@fortics.com.br>
2020-07-20 12:30:18 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -34,6 +34,7 @@ export class SignUpController implements Controller {
       const account = await this.addAccount.add({ name, email, password })
       return created(account)
     } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
