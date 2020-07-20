4db7724152eb71ec029222e74e47a9e9b8ae517c
Felipe <felipe.martins@fortics.com.br>
2020-07-20 13:13:37 -0300

    test: create EmailValidatorAdapter

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,7 @@
+// import { EmailValidatorAdapter } from './email-validator-adapter'
+
+describe('EmailValidator Adapter', () => {
+  test('Should retirn false if validator returns false', () => {
+    // const sut = new EmailValidatorAdapter()
+  })
+})
