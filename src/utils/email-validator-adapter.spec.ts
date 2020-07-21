6b7d914f994dd0d15351f6db50c949b5e6f2b549
Felipe <felipe.martins@fortics.com.br>
2020-07-21 18:42:03 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,7 +1,11 @@
-// import { EmailValidatorAdapter } from './email-validator-adapter'
+import { EmailValidatorAdapter } from './email-validator'
 
 describe('EmailValidator Adapter', () => {
   test('Should retirn false if validator returns false', () => {
-    // const sut = new EmailValidatorAdapter()
+    const sut = new EmailValidatorAdapter()
+
+    const isValid = sut.isValid('invalid_email.com')
+
+    expect(isValid).toBe(false)
   })
 })
