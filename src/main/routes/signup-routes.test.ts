b359e8e10b2bbd8b0f9ecbfbe147f21b3b9838fd
Felipe Romão <feliperomaocad@gmail.com>
2020-07-28 23:02:19 -0300

    refactor: connect to mongodb before tests and disconnect after it

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  // ? Este etapa do teste tem como objetivo limpar a tabela(collection) do banco de dados entre cada teste
+  beforeEach(async () => {
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
