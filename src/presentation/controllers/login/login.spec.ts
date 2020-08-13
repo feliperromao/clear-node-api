b40d36dbfacee72f686414a77b9d1e8789ada275
Felipe Romão <feliperomaocad@gmail.com>
2020-08-12 21:53:21 -0300

    test: ensure LoginController returns 400 if password is no provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,12 +3,12 @@ import { HttpRequest } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../erros'
 
-const makeFakeRequest = (): HttpRequest => ({
-  body: {
-    email: 'felipe@mail.com',
-    password: '123456'
-  }
-})
+// const makeFakeRequest = (): HttpRequest => ({
+//   body: {
+//     email: 'felipe@mail.com',
+//     password: '123456'
+//   }
+// })
 
 interface SutTypes {
   sut: LoginController
@@ -22,8 +22,25 @@ const makeSut = (): SutTypes => {
 describe('Login Controller', () => {
   test('Should return 400 if email is no provided', async () => {
     const { sut } = makeSut()
-    const httpResponse = await sut.handle(makeFakeRequest())
+    const httpRequest: HttpRequest = {
+      body: {
+        password: '123456'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if password is no provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'felipe@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 })
